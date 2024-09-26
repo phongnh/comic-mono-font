34476a795e7bd6ecd4ffe237ea57415fcaebf7c6
Phong Nguyen <nhphong1406@gmail.com>
2024-09-26 18:37:22 +0700

    Generate SemiBold fonts

--- build-v1.sh
+++ build-v1.sh
@@ -2,5 +2,7 @@
 
 rm -f ./ComicMono*.ttf
 fontforge ./generate.py vendor/comic-shanns.otf
+fontforge ./generate-semibold.py vendor/comic-shanns.otf
 [ -s vendor/IosevkaLarge-Regular.ttf ] && fontforge ./generate-large.py vendor/comic-shanns.otf
+[ -s vendor/IosevkaLarge-Regular.ttf ] && fontforge ./generate-large-semibold.py vendor/comic-shanns.otf
 ./copy-fonts.sh
--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 rm -f ./ComicMono*.ttf
 fontforge ./generate.py vendor/comic-shanns-v2.otf
+fontforge ./generate-semibold.py vendor/comic-shanns-v2.otf
 [ -s vendor/IosevkaLarge-Regular.ttf ] && fontforge ./generate-large.py vendor/comic-shanns-v2.otf
+[ -s vendor/IosevkaLarge-Regular.ttf ] && fontforge ./generate-large-semibold.py vendor/comic-shanns-v2.otf
 ./copy-fonts.sh
